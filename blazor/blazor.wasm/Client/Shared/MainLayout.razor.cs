8b99192209d2d83f14c8449c4705ab95cab7385a
daehyun.kim <zndgocu@gmail.com>
2022-09-07 20:27:43 +0900

    npm 추가 전

---
+++ blazor/blazor.wasm/Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace blazor.wasm.Client.Shared
+{
+    public partial class MainLayout
+    {
+        private AppBar _appBar;
+        private bool _toggle { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+        private void OnToggleChanged(bool toggle)
+        {
+            _toggle = toggle;
+        }
+    }
+}
